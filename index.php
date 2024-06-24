cf395c8139c899709e3d5ccfbaea52eed3d741f7
Anna Santana <annacarol04.santana@gmail.com>
2024-06-24 10:30:33 -0300

    erro

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./index.css">
+  
+    <title>Lenny Coser</title>
+</head>
+<body>
+    <header>
+           
+              <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid w-100">
+                  <a class="navbar-brand" href="#">Home</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Sobre</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Portfólio</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Serviços</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Blog</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="contacto.php">Contatos</a>
+                      </li>
+                      
+                    </ul>
+      
+                  </div>
+                </div>
+              </nav>
+              
+            
+    </header>
+
+    
+  <div class="lifestyle-imagem">
+    <div class="content">
+      <img class="lifestyle-imagens" src="./imagens/foto3.jpg" alt="Imagem de casal em preto e branco">
+      <div class="text-lifestyle">
+        <h2>FOTOGRAFIA LIFESTYLE</h2>
+        <p>A fotografia lifestyle tem como intenção principal registrar o que tem de mais verdadeiro numa cena.
+          Existe sim uma “direção de modelos”, uma escolha da locação, ver a posição pra ficar melhor com a luz, mas a gente quer capturar sentimentos e expressões que são típicos daquela pessoa que está sendo fotografada, algo que seja bem o jeitinho da família que está ali sendo retratada.</p>
+        <button type="button" class="btn btn-outline-primary">GALERIAS</button> 
+        <button type="button" class="btn btn-outline-primary">CONTACTAR</button>
+        <h4>“A MELHOR COISA DE UMA IMAGEM É QUE NUNCA MUDA, MESMO QUANDO AS PESSOAS NELA MUDAM."</h4>  
+      </div>
+    </div>
+  </div>
+  
+<div class="image-center">
+    <img class="center-imagem" src="./imagens/mae-bebe.jpg" alt="Imagem mãe com bebe">
+</div>
+
+</body>
+</html>
